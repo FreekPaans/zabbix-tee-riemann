b22dac15f9e82c3802ee05b443219258f6cb3195
Freek Paans <freekpaans@gmail.com>
2017-11-05 20:26:38 +0100

    lein new zabbix-tee-riemann

---
+++ src/zabbix_tee_riemann/core.clj
@@ -0,0 +1,6 @@
+(ns zabbix-tee-riemann.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/zabbix_tee_riemann/core_test.clj
@@ -0,0 +1,7 @@
+(ns zabbix-tee-riemann.core-test
+  (:require [clojure.test :refer :all]
+            [zabbix-tee-riemann.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
