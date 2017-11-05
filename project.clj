e344c804d61c2f948d98b8a432586596deecffc2
Freek Paans <freekpaans@gmail.com>
2017-11-05 22:58:28 +0100

    aot only for uberjar

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :main zabbix-tee-riemann.core
-  :aot [zabbix-tee-riemann.core]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.netty/netty-all "4.1.16.Final"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]]
+  :profiles {:uberjar {:aot :all}})
